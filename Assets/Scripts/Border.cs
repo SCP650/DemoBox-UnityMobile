fba2abf992905adbbdf1fff4a0187b6d05cabb2c
Sebastian Yang <yukaiy@andrew.cmu.edu>
2019-10-16 14:37:26 -0400

    Starter Code

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    [Tooltip("Up, Down, Left or Right")]
+    [SerializeField]
+    string location;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        float ScreenHeight = 2*Camera.main.orthographicSize;
+        float ScreenWidth = ScreenHeight * Camera.main.aspect;
+        float width = 0.5f;
+
+        float up = ScreenHeight/2 + width/2;
+        float down = -ScreenHeight/2 - width/2;
+        float left = -ScreenWidth/2 - width/2;
+        float right = ScreenWidth/2 + width/2;
+
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+
+        //Change location and Scale cooresponding to camera size
+        switch (location)
+        {
+            case "Up":
+                transform.position = new Vector3(x, up, z);
+                transform.localScale = new Vector3(ScreenWidth, width ,z);
+                break;
+            case "Left":
+                transform.position = new Vector3(left, y, z);
+                transform.localScale = new Vector3(width, ScreenHeight, z);
+                break;
+            case "Right":
+                transform.position = new Vector3(right, y, z);
+                transform.localScale = new Vector3(width, ScreenHeight, z);
+                break;
+            case "Down":
+                transform.position = new Vector3(x, down, z);
+                transform.localScale = new Vector3(ScreenWidth, width, z);
+                break;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Slider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+
+    private bool IsSelected;
+    private float ScreenWidth;
+    private float ScreenHeight;
+    private float SelfSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsSelected = false;
+        ScreenHeight = 2f * Camera.main.orthographicSize;
+        ScreenWidth = ScreenHeight * Camera.main.aspect;
+        SelfSize = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            IsSelected = true;
+            float x = Mathf.Clamp(mouse.x, -ScreenWidth/2 + SelfSize, ScreenWidth/2 - SelfSize);
+            transform.position = new Vector3(x ,transform.position.y,0);
+            
+        }
+ 
+
+    }
+
+}
