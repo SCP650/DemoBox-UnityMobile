ee173a3bd28c15f028a4d35458e5608c807f6a19
Sebastian Yang <yukaiy@andrew.cmu.edu>
2019-10-16 15:09:49 -0400

    Ball Movement Added

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 100.0f;
+    // Update is called once per frame
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+}
+
